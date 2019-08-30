4a4c725ac3e83a7b2d77ff9f66280ea4e07a21b2
Alexander Sergeev <sanekas@yandex-team.ru>
2019-08-30 11:43:39 +0300

    fix test

--- src/test/java/edu/sanekas/moneytransfer/storages/AppendableInMemoryAccountsStorageTest.java
+++ src/test/java/edu/sanekas/moneytransfer/storages/AppendableInMemoryAccountsStorageTest.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AppendableInMemoryAccountsStorageTest {
-    private final AccountsStorage storage = new AppendableInMemoryAccountsStorage();
+    private static final AccountsStorage storage = new AppendableInMemoryAccountsStorage();
 
     @Test
     public void testNonExistedAccount() {
